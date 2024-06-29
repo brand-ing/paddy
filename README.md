51c1bfe72182b73943e249050f3759338059a802
chrome <brandontirving@gmail.com>
2024-06-29 11:33:56 -0400

    Restructuring

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Paddy is a password manager that works differently.
 # Update
 
 New stuff
+
+Password Generation
+
+Password Security Checker
+
+
+# Coming soon
+
+Password Storage
+Peak
+Autofill
+
---
+++ passman/main.py
@@ -0,0 +1,5 @@
+
+
+
+# Need to restructure the project and redescover our focus and objectives. 
+# I want it to at least be able to generate and verify and make suggestions on passwords before next coaching.
